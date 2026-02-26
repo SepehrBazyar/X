1128ff4168b39f1246f0a9ac67a593acefe16f70
Sepehr Bazyar <sepehr.bazyar@karnameh.ir>
2026-02-26 16:53:09 +0330

    feat: implement docker files

--- X/settings.py
+++ X/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'X.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'X',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '0.0.0.0',
+        'PORT': 5432,
     }
 }
 
